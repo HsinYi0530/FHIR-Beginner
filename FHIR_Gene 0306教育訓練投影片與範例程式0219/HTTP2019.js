1d202a04a0aab6d314162644d5510fe13403711c
HsinYiLin <69176047+HsinYi0530@users.noreply.github.com>
2021-02-22 21:56:09 +0800

    Add files via upload

---
+++ FHIR_Gene 0306教育訓練投影片與範例程式0219/HTTP2019.js
@@ -0,0 +1,26 @@
+﻿//取得網路上的資源
+function HTTPGetData(urlStr) {
+    var HttpObj = new XMLHttpRequest();   
+    HttpObj.onreadystatechange = function () {
+        if (HttpObj.readyState === 4) {
+            var ret = HttpObj.responseText;
+            return ret;
+        }
+    }
+	HttpObj.open("GET", urlStr, true);
+    HttpObj.send();
+}
+
+//上傳 dataStr 到網路上
+function HTTPPostData(urlStr, dataStr ) {
+    var HttpObj = new XMLHttpRequest();
+	HttpObj.open("POST", urlStr, true);
+    HttpObj.setRequestHeader("Content-type", "application/json+fhir");
+    HttpObj.onreadystatechange = function () {
+        if (HttpObj.readyState === 4) {
+            ret = HttpObj.responseText;
+            alert(ret);
+        }
+    }
+    HttpObj.send(dataStr);
+}
